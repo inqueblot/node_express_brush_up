ba3a1cb5d764085ff9192fc89f6bc653bed515ef
inqueblot <notminebydesign@gmail.com>
2021-04-10 19:05:26 -0500

    node built-in modules

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+// Modules
+const { john, peter } = require('./4-names');
+const sayHi = require('./5-utils');
+const data = require('./6-alternative-flavor')
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
