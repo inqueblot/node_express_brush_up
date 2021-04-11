15e08ccbdc27421162c769e4cff635d3a44c4fa8
inqueblot <notminebydesign@gmail.com>
2021-04-11 00:37:47 -0500

    http module

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const { readFileSync, writeFileSync } = require('fs');
+const http = require('http')
 
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to our home page')
+    }
+    else if (req.url === '/about') {
+        res.end('Here is our short history')
+    } else {
+        res.end(`
+    <h1>Oops!</h1>
+     <p>We can't see to find the page you're looking for</p>
+     <a href="/">back home</a>
+     `)
+    }
+})
 
-writeFileSync(
-    './content/result-sync.txt',
-    `Here is the result: ${first}, ${second}`,
-    { flag: 'a' }
-)
+server.listen(5000)
