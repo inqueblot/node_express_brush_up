81b6eebe61b256145ba096a6251a1cd1be7ff9fa
inqueblot <notminebydesign@gmail.com>
2021-04-16 02:03:37 -0500

    basic express app

--- node-express-course/2-express-tutorial/final/3-express-basics.js
+++ node-express-course/2-express-tutorial/final/3-express-basics.js
@@ -1,21 +1,24 @@
-const express = require('express')
-const app = express()
+const app = require('express')();
 
 app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
+  console.log('home page');
+  res
+    .status(200)
+    .send('Home Page')
 })
 
 app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
+  res.send('About Page')
 })
 
 app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
+  res
+    .status(404)
+    .send('<h1>resource not found</h1>')
 })
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
+  console.log('server is listening on port 5000...');
 })
 
 // app.get
--- node-express-course/2-express-tutorial/final/4-express-app.js
+++ node-express-course/2-express-tutorial/final/4-express-app.js
@@ -1,19 +1,24 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 const path = require('path')
 
-const app = express()
+const port = 5000
 
-// setup static and middleware
+//setup static and middleware
 app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+//not needed if served in public folder
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
 
 app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
+  res
+    .status(404)
+    .send('resource not found')
 })
 
-app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
+
+app.listen(port, () => {
+  console.log(`server is listening on port: ${port}...`);
 })
