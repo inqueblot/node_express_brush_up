cf25b421bb05a841e3e2919ca4468da3667dd39b
inqueblot <notminebydesign@gmail.com>
2021-04-19 01:39:39 -0500

    put and delete route

--- node-express-course/2-express-tutorial/app.js
+++ node-express-course/2-express-tutorial/app.js
@@ -43,6 +43,37 @@ app.post('/api/people', (req, res) => {
     res.status(201).json({ success: true, person: name })
 })
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    console.log(id, name);
+    const person = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        return res.status(404)
+            .json({ success: false, msg: `no person with ID ${id}` })
+    }
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({ success: true, data: newPeople })
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+
+    if (!person) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `no person with ID ${req.params.id}` })
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id));
+    return res.status(200).json({ success: true, data: newPeople })
+})
+
 app.listen(5000, () => {
     console.log('app is listening on port 5000...');
 })
